9475594b07b6447f6903a5648b755255ce50595b
Gage <gagelangdon@gmail.com>
2017-06-28 11:36:34 -0700

    add routing

---
+++ src/shared/404.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class FourOFour extends Component {
+    static fetchData() {
+        console.log('test');
+    }
+    render() {
+        return (
+            <div>
+                404; page not found
+            </div>
+        );
+    }
+}
+export default FourOFour;
---
+++ src/shared/user.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class User extends Component {
+    static fetchData() {
+        console.log('test');
+    }
+    render() {
+        return (
+            <div>
+                User
+            </div>
+        );
+    }
+}
+export default User;
